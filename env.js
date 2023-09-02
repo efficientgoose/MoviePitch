b1207eabf34bbe71ce1ca396ec9696db41400ff9
Ajinkya Kale <127986789+ajinkode@users.noreply.github.com>
2023-09-02 16:25:48 +0530

    Remove env.js

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 export const process = {
     env: {
-        OPENAI_API_KEY: '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'
+        OPENAI_API_KEY: 'sk-xxxx' //your api key goes here
     }
 }
